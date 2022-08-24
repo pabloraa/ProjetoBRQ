9aea6cbe3751b0c4c65c6ed3eb76c19733282473
pabloraa <pabloraa@hotmail.com>
2022-08-23 23:17:38 -0300

    Versao 24-08

--- WebApiModels/Models/Validacao/ContaValidator.cs
+++ WebApiModels/Models/Validacao/ContaValidator.cs
@@ -2,24 +2,41 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using WebApi.Models;
 
 namespace WebApiModels.Models.Validacao
 {
     public class ContaValidator : AbstractValidator<Conta>
     {
+        private const string expressao = @"[0-9]";
+
         public ContaValidator()
         {
             RuleFor(x => x.Agencia)
                 .NotNull().WithMessage("Agência não pode ser nulo!")
                 .GreaterThan(0).WithMessage("Agência não pode ser negativa!")
+                .Must(ValidarNumeros).WithMessage("Somente número são aceitos")
                 .NotEmpty().WithMessage("Agência não pode ser nula!");
             RuleFor(x => x.NumeroConta)
                 .NotNull().WithMessage("O número da conta não pode ser nulo!")
                 .GreaterThan(0).WithMessage("O número da conta não pode ser negativo!")
+                .Must(ValidarNumeros).WithMessage("Somente números são aceitos")
                 .NotEmpty().WithMessage("O número da conta não pode ser nulo!");
             RuleFor(x => x.Ativo)
                 .NotNull().WithMessage("Conta ativa = true / Conta inativa = false");
+        }
+
+        public static bool ValidarNumeros(int numero)
+        {
+            Regex num = new Regex(expressao);
+
+            //if (numero.Equals(num))
+            //    return false;
+            //return true;
+
+            return numero.Equals(num)?false:true;
+            //return (new Regex(expressao).IsMatch(numero.ToString()));
         }
     }
 }
